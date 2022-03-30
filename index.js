7a3cc3fd4bcc2a3048b8dc4ad7ce1dda3e104f4b
Natto <70090705+zx4545zx@users.noreply.github.com>
2022-03-30 19:38:45 +0700

    Able use dynamic route

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import Home from './pages/Home.js'
 import About from './pages/About.js'
 import Register from './pages/Register.js'
+import User from './pages/User.js'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
   { path: '/register', component: Register },
+  { path: '/users/:id', component: User },
 ]
 
 const router = VueRouter.createRouter({
@@ -13,7 +15,13 @@ const router = VueRouter.createRouter({
   routes, // short for `routes: routes`
 })
 
-const app = Vue.createApp({})
+const app = Vue.createApp({
+  data() {
+    return {
+      id: 1
+    }
+  }
+})
 
 app.use(router)
 app.mount('#app')
---
+++ pages/User.js
@@ -0,0 +1,10 @@
+const User = //html
+`
+<div>
+  <h1 class="text-center">User ID: {{ $route.params.id }}</h1>
+</div>
+`
+
+export default {
+  template: User,
+}
